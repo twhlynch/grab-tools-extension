a3ea8a783d7d58c8d5e04be56a32a1c934099470
Tom Lynch <twhlynch@gmail.com>
2023-11-06 14:16:52 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const domain = window.location.hostname;
+const path = window.location.pathname;
+const query = window.location.search;
+console.log(`Domain: ${domain}\nPath: ${path}\nQuery: ${query}`);
+
+if (domain === "grab-tools.live") {
+    grabToolsScript();
+} else if (domain === "grabvr.quest") {
+    grabVRScript();
+}
+
+function grabToolsScript() {
+
+}
+
+function grabVRScript() {
+    if (path === "/levels" || path === "/levels/") {
+        const observer = new MutationObserver(handleMutations);
+        observer.observe(document.body, {childList: true, subtree: true});
+    } else if (path === "/levels/viewer" || path === "/levels/viewer/") {
+        const levelId = query.split("?level=")[1];
+        document.body.innerHTML += `
+            <div class="gtl-buttons">
+                <a class="gtl-btn" href="https://grab-tools.live/editor?level=${levelId}">Edit</a>
+                <a class="gtl-btn" href="https://grab-tools.live/download?level=${levelId}">Download</a>
+            </div>    
+        `
+    }
+}
+
+function addButtonsToCard(element) {
+    const card = element.childNodes[0];
+    const playButton = card.childNodes[13];
+    const levelUrl = playButton.getAttribute("href");
+    const levelId = levelUrl.split("?level=")[1];
+    card.innerHTML += `
+        <a class="gtl-btn" href="https://grab-tools.live/editor?level=${levelId}">Edit</a>
+        <a class="gtl-btn" href="https://grab-tools.live/download?level=${levelId}">Download</a>
+    `
+}
+
+function handleMutations(mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.type === "childList") {
+            mutation.addedNodes.forEach(function (node) {
+                if (node.classList && node.classList.contains("grid-item")) {
+                    addButtonsToCard(node);
+                }
+            });
+        }
+    });
+}
